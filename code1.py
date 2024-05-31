c326e14aff8786bbfa0288a3260c68d9f2dfc598
asherbabu <88152482+asherbabu@users.noreply.github.com>
2024-05-31 12:03:52 +0530

    delted code.ipynb

--- code1.py
+++ code1.py
@@ -3,10 +3,8 @@ import requests
 def fetchAndSaveToFile(url, path):
     r = requests.get(url)
     with open(path, "w") as f:
-        f. write(r.text)
+        f.write(r.text)
 
 url = "https://timesofindia.indiatimes.com/city/delhi"
 
-r = requests.get(url)
-
-print(r.text)
+fetchAndSaveToFile(url, "data/times1.html")
