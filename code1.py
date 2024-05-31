dcf72b40dee090c909a65605d83bb311d1a8986c
asherbabu <88152482+asherbabu@users.noreply.github.com>
2024-05-31 12:10:58 +0530

    function executed successfully

--- code1.py
+++ code1.py
@@ -2,9 +2,9 @@ import requests
 
 def fetchAndSaveToFile(url, path):
     r = requests.get(url)
-    with open(path, "w") as f:
+    with open(path, "w", encoding="utf-8") as f:
         f.write(r.text)
 
 url = "https://timesofindia.indiatimes.com/city/delhi"
 
-fetchAndSaveToFile(url, "data/times1.html")
+fetchAndSaveToFile(url, "data/times.html")
